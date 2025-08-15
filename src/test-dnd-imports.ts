3dd721d052c1cc68e27f98f1ca491e0a48603574
Hayato <jolly1738810@gmail.com>
2025-08-15 05:00:14 +0000

    Edited Spark

---
+++ src/test-dnd-imports.ts
@@ -0,0 +1,8 @@
+// This is a test to verify react-dnd imports are working
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
+// If this file can be created without errors, react-d
+
+// If this file can be created without errors, react-dnd is properly installed
+console.log('React DnD imports successful')
+
+export const testImports = true
